31f886f4643cd24742bc4729adb63b183464eecf
howcleiton <cleiton_2228@hotmail.com>
2025-06-22 01:41:49 -0300

    fix bugs para deplou netilify

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,16 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+// Correção para suportar __dirname em ESM (ambiente Netlify incluso)
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Configuração Vite
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, 'src'), // 👈 Corrigido para 'src' (sem './')
     },
   },
 });
