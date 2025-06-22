2daca426bf0ef39ee9135a9677b22cbcbaf4bef0
howcleiton <cleiton_2228@hotmail.com>
2025-06-22 01:26:21 -0300

    para deploy no netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path'; // 👈 adicionar isso
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-// https://vitejs.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // 👈 configurar o alias
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
